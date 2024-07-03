f379608b5af2a58bfd19365acb04f70f9ed2fac3
Omprakash Prajapati <oprajapati249@gmail.com>
2024-07-03 17:00:24 +0530

    log

--- components/B2b/CompanyLogoSlider.jsx
+++ components/B2b/CompanyLogoSlider.jsx
@@ -2,7 +2,6 @@ import { CompanyLogo1 } from "@public";
 import Image from "next/image";
 
 export default function CompanyLogoSlider() {
-  console.log("hi");
   return (
     <section className="bg-primary-two pt-10 xl:pt-20">
       <div className="bg-primary-one py-16 overflow-hidden relative whitespace-nowrap">
